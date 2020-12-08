d342bd9a940e98388a99e2504dde4f20c51923a8
XTT-20201014ZEH\Administrator <wenhao.yu@my.edu.jcu.au>
2020-12-08 14:31:26 +0800

    add list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,12 @@ print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the number is {}".format(sum(numbers) / len(numbers)))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+user_name = input("Enter your username: ")
+if user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
