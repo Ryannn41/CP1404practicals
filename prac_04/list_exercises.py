c72da8a41c32d6100cb0a03529752cb3fd27f640
XTT-20201014ZEH\Administrator <wenhao.yu@my.edu.jcu.au>
2020-12-08 14:31:35 +0800

    add list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -14,11 +14,11 @@ print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the number is {}".format(sum(numbers) / len(numbers)))
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 user_name = input("Enter your username: ")
 if user_name in usernames:
     print("Access granted")
 else:
     print("Access denied")
-
