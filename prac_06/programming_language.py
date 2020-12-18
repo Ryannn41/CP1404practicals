fb2ad27593c9d034862b45838b49f586d4e65e30
XTT-20201014ZEH\Administrator <wenhao.yu@my.edu.jcu.au>
2020-12-18 17:04:40 +0800

    add languages.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,2 +1,10 @@
 class ProgrammingLanguage:
-    pass
+
+    def __init__(self):
+        self.name = name
+        self.type = type
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return "{}, {}, Reflection={}, First appeared in {}".format(self.name, self.type, self.reflection, self.year)
