96dd20f3fcf97c4dd9e899ca530f43f2aef497a0
XTT-20201014ZEH\Administrator <wenhao.yu@my.edu.jcu.au>
2021-01-03 13:19:09 +0800

    modified squaring int to float

--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -23,7 +23,7 @@ class SquareNumberApp(App):
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
+        result = float(value ** 2)
         self.root.ids.output_label.text = str(result)
 
 
